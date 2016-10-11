9d438970a7715082c75603e1239573ba0a18565c
Jan <karchej@NB082011.intra.net.kvbw>
2016-10-11 10:28:51 +0200

    m

--- E3FI3_AE/src/Jan/gui/AWTDemo.java
+++ E3FI3_AE/src/Jan/gui/AWTDemo.java
@@ -2,15 +2,30 @@ package Jan.gui;
 
 import java.awt.Button;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+class MyListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Aua, das tat weh!");
+	}
+	
+}
 public class AWTDemo {
 	public static void main(String[] args) {
 		// Nimmt Swing nicht AWT... AWT suxxs
+		
+		MyListener myL = new MyListener();
+		
+		
 		Frame fenster = new Frame("Testfenster");
 		fenster.setSize(300, 200);
 		
 		
 		Button btn = new Button("Lolo");
+		btn.addActionListener(myL);
 		
 		fenster.add(btn);
 		fenster.setVisible(true);
