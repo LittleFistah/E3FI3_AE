128fd3757f1be861d6d1f670b4de4d9f72cc90eb
Jan <karchej@NB082011.intra.net.kvbw>
2016-10-11 10:20:44 +0200

    a

--- E3FI3_AE/src/Jan/gui/AWTDemo.java
+++ E3FI3_AE/src/Jan/gui/AWTDemo.java
@@ -1,11 +1,17 @@
 package Jan.gui;
 
+import java.awt.Button;
 import java.awt.Frame;
 
 public class AWTDemo {
 	public static void main(String[] args) {
 		Frame fenster = new Frame("Testfenster");
 		fenster.setSize(300, 200);
+		
+		
+		Button btn = new Button("Lolo");
+		
+		
 		fenster.setVisible(true);
 		
 		System.out.println("Ende!");
