9cfaa9f9c191ee215ec7ab95d821959029c316a2
Jan <karchej@NB082011.intra.net.kvbw>
2016-10-20 12:33:47 +0200

    aa

--- E3FI3_AE/src/Jan/blinkekreis/Gui.java
+++ E3FI3_AE/src/Jan/blinkekreis/Gui.java
@@ -4,29 +4,31 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-public class Gui extends JFrame{
+public class Gui extends JFrame {
 
 	private Color color;
 	private Thread blinker;
 	private boolean isBlinking = true;
-	
+
 	public Gui() {
 		init();
 	}
-	
-	private void init(){
+
+	private void init() {
 		super.setTitle("Blinkekreis");
 		super.setSize(320, 320);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setResizable(false);
 		super.setVisible(true);
 		blinker = new Thread() {
 			@Override
 			public void run() {
-				do{
-					if(color != Color.YELLOW){
+				do {
+					if (color != Color.YELLOW) {
 						color = Color.YELLOW;
-					}else {
+					} else {
 						color = Color.BLUE;
 					}
 					try {
@@ -34,18 +36,23 @@ public class Gui extends JFrame{
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					repaint();
-				}while(isBlinking);
+					SwingUtilities.invokeLater(new Runnable() {
+						@Override
+						public void run() {
+							repaint();
+						}
+					});
+				} while (isBlinking);
 				super.run();
 			}
 		};
 		blinker.start();
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(color);
-		g.fillOval(10, 20, 300, 300);
+		g.fillOval(10, 30, 300, 280);
 	}
 }
